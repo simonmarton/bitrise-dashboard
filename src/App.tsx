d75666f4babe13f86dee26b657e798e9f95e1e3f
Simon Márton <simon.marton@bitrise.io>
2021-01-26 22:39:56 +0100

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { ReactQueryDevtools } from 'react-query-devtools';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Flex } from '@bitrise/bitkit';
 
 import BuildList from './components/BuildList';
 
 const App = () => (
   <Flex padding="x12" direction="vertical">
-    <BuildList />
+    <Router>
+      <Switch>
+        <Route path="/:buildId">
+          <div>build details..</div>
+        </Route>
+        <Route path="/">
+          <BuildList />
+        </Route>
+      </Switch>
+    </Router>
     <ReactQueryDevtools />
   </Flex>
 );
